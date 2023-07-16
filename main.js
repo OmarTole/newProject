3c78097179b6ed5eae413e6ab7500ce94f8cfe69
Tole Bi <omartole23@gmail.com>
2023-07-16 18:37:52 +0600

    first programm in learning udemy

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello Tole BI");
+let number0fFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+let personalMovieDB = {
+    count : number0fFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false,
+}
+
+let a = prompt('Один из последних просмотренных фильмов?');
+let b = prompt('оценка')
+
+
+personalMovieDB.movies[a] = b;
+
+
+console.log(personalMovieDB);

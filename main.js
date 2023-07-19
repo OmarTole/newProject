d97cf3221e7bbf25f24d5be123829f9d8fd7d59f
Tole Bi <omartole23@gmail.com>
2023-07-19 12:34:26 +0600

    training conditions and cycles

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-let number0fFilms = prompt('Сколько фильмов вы уже посмотрели?');
+'use strict';
 
-let personalMovieDB = {
-    count : number0fFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false,
+const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели')
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов'),
+          b = prompt('На сколько вы его оцените?');
+
+    personalMovieDB.movies[a] = b;
 }
 
-let a = prompt('Один из последних просмотренных фильмов?');
-let b = prompt('оценка')
-
-
-personalMovieDB.movies[a] = b;
-
+if (personalMovieDB.count < 10) {
+    console.log('Вы довольно мало фильмов посмотрели!');
+} else if (personalMovieDB.count > 10) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);

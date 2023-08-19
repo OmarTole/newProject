f0738ad40312acac0ab9072a2a976d6adedf35c8
Tole Bi <omartole23@gmail.com>
2023-08-19 18:15:00 +0600

    exercise metod and array

--- learn.js
+++ learn.js
@@ -1,21 +1,60 @@
 'use strict'
 
-const inputTNG = document.querySelector('.tng'),
-      inputUSD = document.querySelector('.usd');
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
 
-inputTNG.addEventListener('input', () => {
-    const request = new XMLHttpRequest();
+// function showGoodFilms(arr) {
 
-    request.open('GET', 'current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8')
-    request.send();
+//     arr = arr.filter(item => {
+//         return item.rating >= 8;
+//     })
+//     return arr;
+// }
 
-    request.addEventListener('load', () => {
-        const data = JSON.parse(request.response);
-        if (request.status === 200) {
-            inputUSD.value = (inputTNG.value / data.current.usd).toFixed(2);
-        } else {
-            inputUSD.value = 'Что-то пошло не так!'
-        }
+// const showGF = showGoodFilms(films);
+
+
+// function showListOfFilms(arr) {
+//     return arr.reduce((acc, curr) =>`${typeof(acc) === 'object' ? acc.name : acc}, ${curr.name}`);
+// }
+
+// console.log(showListOfFilms(films));
+
+function setFilmsIds(arr) {
+    arr = arr.map((item, i) => {
+        item.id = i++
+        return item
+    });
+    return arr;
+}
+
+console.log(setFilmsIds(films));
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    const result = arr.every(item => {
+        console.log(item);
+        return item.id === 0 || item.id;
     })
-})
+
+    return result;
+}
+
+console.log(checkFilms(tranformedArray));
